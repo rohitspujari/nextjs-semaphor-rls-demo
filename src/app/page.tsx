677bae65679cf35b75156e408f168c673dfedc66
knightcoder <rohitspujari@gmail.com>
2024-09-20 11:55:09 -0500

    replaced dashboard id and secret

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { AuthToken } from 'semaphor';
 import DashboardComponent from './components/dashboard-component';
 import { postRequest } from '@/server-utils';
 
-const DASHBOARD_ID = 'd_cf007a8b-19bc-46ad-8787-2915445b7b86'; // Replace with your actual dashboard ID
-const DASHBOARD_SECRET = 'ds_f32f0b30-b7e1-40f9-ba6a-9804a5b9d635'; // Replace with your actual dashboard secret
+const DASHBOARD_ID = 'd_a1a5275b-5ded-411c-a2bb-bfafd55e7b26'; // Replace with your actual dashboard ID
+const DASHBOARD_SECRET = 'ds_13353da5-e413-46cb-a8af-fd58192aab11'; // Replace with your actual dashboard secret
 const TOKEN_URL = 'https://semaphor.cloud/api/v1/token';
 
 export default async function HomePage() {
